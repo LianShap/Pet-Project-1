c7d0160e0d508c8bf078f8f74173443e060ad100
StanislavStarodubtsev <sstarodubtsev79@gmail.com>
2025-06-23 23:19:32 +0300

    Frontend 0.0.1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo, Dark Horizontal.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='Text'>
           In progress...
         </p>
         <a
