9aabb05972f1a74f0cd44764623dc9f5d4866fe9
Anastasia Shapoval <nastya030207@gmail.com>
2025-06-24 11:57:46 +0300

    Update module.ts

--- backend/src/app/module.ts
+++ backend/src/app/module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller';
 import { AppService } from './service';
+import config from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(config),
   controllers: [AppController],
   providers: [AppService],
 })
